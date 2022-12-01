b8b93dba518351779faf4178a2a7b53ea0da61e4
Rob Lankey <rob.lankey@gmail.com>
2022-12-01 16:03:15 -0500

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,20 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
-    }
+        var calories = 0
+        val elves = mutableListOf<Int>()
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+        input.forEach {
+            if (it.isEmpty()) {
+                elves.add(calories)
+                calories = 0
+            } else {
+                calories += it.toInt()
+            }
+        }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        return elves.max()
+    }
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
 }
